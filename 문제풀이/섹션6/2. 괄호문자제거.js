ca0393261b86aec4e91c3560da17822ef7e3d9de
yejin kim <yejin0216@gmail.com>
2022-05-25 21:43:26 +0900

    문제풀이(25/May/21)

--- 문제풀이/섹션6/2. 괄호문자제거.js
+++ 문제풀이/섹션6/2. 괄호문자제거.js
@@ -1,22 +1,31 @@
 function solution(s){
-    const sArr = [...s];
-    const temp = [];
+    const stack = [];
 
-    for ( let i=0; i<sArr.length; i++ ) {
-        if ( sArr[i] !== ")" ) {
-            temp.push(sArr[i]);
-        } else {
-            for ( let j=temp.length-1; j>=0; j-- ) {
-                if ( temp[j] !== '(' ) {
-                    temp.pop();
-                } else {
-                    temp.pop();
-                    break;
-                }
-            }
-        }
+    for (const x of s) {
+        if (x === ')') {
+            while(stack.pop() !== '(');
+        } else stack.push(x);
     }
-    return temp.join("");
+
+    return stack.join("")
+
+    // const temp = []
+    // const sArr = [...s];
+    // for ( let i=0; i<sArr.length; i++ ) {
+    //     if ( sArr[i] !== ")" ) {
+    //         temp.push(sArr[i]);
+    //     } else {
+    //         for ( let j=temp.length-1; j>=0; j-- ) {
+    //             if ( temp[j] !== '(' ) {
+    //                 temp.pop();
+    //             } else {
+    //                 temp.pop();
+    //                 break;
+    //             }
+    //         }
+    //     }
+    // }
+    // return temp.join("");
 }
 
 let str="(A(BC)D)EF(G(H)(IJ)K)LM(N)";
