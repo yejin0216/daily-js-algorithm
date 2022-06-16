70d201cdf779297015c9be39fef6ba9e1e44e4bb
yejin kim <yejin0216@gmail.com>
2022-06-16 20:52:58 +0900

    문제풀이(16/JUN/21)

--- leetcode/sorting/Sorting.js
+++ leetcode/sorting/Sorting.js
@@ -56,6 +56,34 @@ const InsertionSorting = (nums) => {
     return nums;
 }
 
-console.log(InsertionSorting([9,3,5,7,1]))
+// console.log(InsertionSorting([9,3,5,7,1]))
 
 // 3. Merge Sorting
+
+// Counting Sorting O(n+k) 정렬할 때 추가적인 메모리(숫자 개수를 저장할 공간, 결과를 저장할 공간)가 필요하다는 점과, 가장 큰 숫자에 영향을 받는다는 점은 단점입니다
+const CountingSorting = (nums) => {
+    const max = Math.max(...nums);
+    const min = Math.min(...nums);
+    const sumArray = Array.from({length: max - min + 1}, () => 0);
+
+    for (let i=0; i<nums.length; i++) {
+        sumArray[nums[i]-min] += 1;
+    }
+
+    const accArray = [sumArray[0]];
+    for (let i=1; i<nums.length; i++) {
+        accArray.push(accArray[i-1] + sumArray[i]);
+    }
+
+    const result = Array.from({length: max - min + 1}, () => 0);
+    for ( let i=nums.length-1; i>=0; i-- ) {
+        const idx = accArray[nums[i]-min];
+        result[idx-1] = nums[i];
+        accArray[nums[i]-min] -= 1;
+    }
+
+    return result;
+}
+
+console.log(CountingSorting([3,5,5,10,1,5,3,2,1,10]))
+
