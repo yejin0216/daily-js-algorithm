28a971c65dc78bf0249b668ea20ddbb822cbe703
yejin kim <yejin0216@gmail.com>
2022-06-19 21:03:15 +0900

    문제풀이(19/JUN/21)

--- leetcode/stack/easy/20.valid-parentheses.js
+++ leetcode/stack/easy/20.valid-parentheses.js
@@ -2,27 +2,47 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    const strArr = s.split("");
-    const temp = [];
+// var isValid = function(s) {
+//     const strArr = s.split("");
+//     const temp = [];
+//
+//     for ( let i=0; i<strArr.length; i++ ) {
+//         const str = strArr[i];
+//         if ( str === '[' || str === '(' || str === '{' ) {
+//             temp.push(str);
+//         } else {
+//             const prev = temp[temp.length-1];
+//             if ( !prev ) return false;
+//             if ( (str === ']' && prev === '[') || (str === '}' && prev === '{') || (str === ')' && prev === '(') ) {
+//                 temp.pop();
+//             } else {
+//                 temp.push(str);
+//             }
+//         }
+//     }
+//
+//     return temp.length === 0;
+// };
 
-    for ( let i=0; i<strArr.length; i++ ) {
-        const str = strArr[i];
-        if ( str === '[' || str === '(' || str === '{' ) {
-            temp.push(str);
-        } else {
-            const prev = temp[temp.length-1];
-            if ( !prev ) return false;
-            if ( (str === ']' && prev === '[') || (str === '}' && prev === '{') || (str === ')' && prev === '(') ) {
-                temp.pop();
-            } else {
-                temp.push(str);
-            }
+var isValid = function(s) {
+    const str = s.split("");
+    const temp  = [];
+
+    for ( let i=0; i<str.length; i++ ) {
+        const c = str[i];
+        if ( c === '(' ) {
+            temp.push(')');
+        } else if ( c === '{' ) {
+            temp.push('}');
+        } else if ( c === '[' ) {
+            temp.push(']');
+        } else if ( temp.length === 0 || temp.pop() !== c ) {
+            return false;
         }
     }
 
     return temp.length === 0;
-};
+}
 
 console.log(isValid("()"))
 console.log(isValid("()[]{}"))
