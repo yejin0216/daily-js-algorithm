b391c35964437c7ff3e55f6e0ad1e51d373173ae
yejin kim <yejin0216@gmail.com>
2022-07-04 22:19:48 +0900

    문제풀이(4/JUL/22)

---
+++ leetcode/hashmap/medium/692.top-k-frequent-words.js
@@ -0,0 +1,47 @@
+/**
+ * @param {string[]} words
+ * @param {number} k
+ * @return {string[]}
+ */
+var topKFrequent = function (words, k) {
+  const map = new Map();
+
+  for (const w of words) {
+    if (map.has(w)) {
+      map.set(w, map.get(w) + 1);
+    } else {
+      map.set(w, 1);
+    }
+  }
+
+  const array = [];
+  for (const [key, value] of map) {
+    array.push([key, value]);
+  }
+
+  const sorted = array.sort((a, b) => {
+    if (a[1] === b[1]) {
+      return a > b ? 1 : -1;
+    }
+    return b[1] - a[1];
+  });
+
+  const result = [];
+  for (const value of sorted) {
+    if (result.length === k) {
+      break;
+    }
+    result.push(value[0]);
+  }
+
+  return result;
+};
+
+console.log(topKFrequent(["i", "love", "leetcode", "i", "love", "coding"], 3));
+// console.log(topKFrequent(["i", "love", "leetcode", "i", "love", "coding"], 2));
+// console.log(
+//   topKFrequent(
+//     ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"],
+//     4
+//   )
+// );
