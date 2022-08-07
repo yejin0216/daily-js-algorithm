cf1d893ebeeef6d3c5489c02ea620876e320b3c2
yejin kim <yejin0216@gmail.com>
2022-08-07 21:38:31 +0900

    문제풀이(7/AUG/21)

--- 문제풀이/섹션6/2. 괄호문자제거.js
+++ 문제풀이/섹션6/2. 괄호문자제거.js
@@ -1,32 +1,42 @@
-function solution(s){
-    const stack = [];
+function solution(s) {
+  // const stack = [];
+  //
+  // for (const x of s) {
+  //   if (x === ")") {
+  //     while (stack.pop() !== "(");
+  //   } else stack.push(x);
+  // }
+  //
+  // return stack.join("");
+  // const temp = []
+  // const sArr = [...s];
+  // for ( let i=0; i<sArr.length; i++ ) {
+  //     if ( sArr[i] !== ")" ) {
+  //         temp.push(sArr[i]);
+  //     } else {
+  //         for ( let j=temp.length-1; j>=0; j-- ) {
+  //             if ( temp[j] !== '(' ) {
+  //                 temp.pop();
+  //             } else {
+  //                 temp.pop();
+  //                 break;
+  //             }
+  //         }
+  //     }
+  // }
+  // return temp.join("");
+  const stack = [];
 
-    for (const x of s) {
-        if (x === ')') {
-            while(stack.pop() !== '(');
-        } else stack.push(x);
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== ")") {
+      stack.push(s[i]);
+    } else {
+      while (stack.pop() !== "(");
     }
+  }
 
-    return stack.join("")
-
-    // const temp = []
-    // const sArr = [...s];
-    // for ( let i=0; i<sArr.length; i++ ) {
-    //     if ( sArr[i] !== ")" ) {
-    //         temp.push(sArr[i]);
-    //     } else {
-    //         for ( let j=temp.length-1; j>=0; j-- ) {
-    //             if ( temp[j] !== '(' ) {
-    //                 temp.pop();
-    //             } else {
-    //                 temp.pop();
-    //                 break;
-    //             }
-    //         }
-    //     }
-    // }
-    // return temp.join("");
+  return stack.join("");
 }
 
-let str="(A(BC)D)EF(G(H)(IJ)K)LM(N)";
+let str = "(A(BC)D)EF(G(H)(IJ)K)LM(N)";
 console.log(solution(str));
