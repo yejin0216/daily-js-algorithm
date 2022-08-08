9cf03ac09eb6a08c9a0386e4802d4e1ec284041e
yejin kim <yejin0216@gmail.com>
2022-08-09 00:02:07 +0900

    문제풀이(8/AUG/21)

--- leetcode/backTracking/medium/17.LetterCombinationsofaPhoneNumber.js
+++ leetcode/backTracking/medium/17.LetterCombinationsofaPhoneNumber.js
@@ -2,36 +2,38 @@
  * @param {string} digits
  * @return {string[]}
  */
-var letterCombinations = function(digits) {
-    const map = new Map();
-    map.set("2", ['a', 'b', 'c']);
-    map.set("3", ['d', 'e', 'f']);
-    map.set("4", ['g', 'h', 'i']);
-    map.set("5", ['j', 'k', 'l']);
-    map.set("6", ['m', 'n', 'o']);
-    map.set("7", ['p', 'q', 'r', 's']);
-    map.set("8", ['t', 'u', 'v']);
-    map.set("9", ['w', 'x', 'y', 'z']);
+var letterCombinations = function (digits) {
+  const map = new Map();
+  map.set("2", ["a", "b", "c"]);
+  map.set("3", ["d", "e", "f"]);
+  map.set("4", ["g", "h", "i"]);
+  map.set("5", ["j", "k", "l"]);
+  map.set("6", ["m", "n", "o"]);
+  map.set("7", ["p", "q", "r", "s"]);
+  map.set("8", ["t", "u", "v"]);
+  map.set("9", ["w", "x", "y", "z"]);
 
-    const digitArr = [...digits];
-    const result = [];
+  const digitArr = [...digits];
+  const result = [];
 
-    function bt(index = 0, letter = "") {
-        const number = digitArr[index];
-        const chars = map.get(number);
+  function bt(index = 0, letter = "") {
+    const number = digitArr[index]; // [a,b,c], [d,e,f], [j,k,l], [m,n,o]
+    // a,d,j,m
+    // a,d,j,n
 
-        if ( index >= digitArr.length ) {
-            result.push(letter);
-        } else {
-            for ( const c of chars ) {
-                bt(index+1, letter + c);
-            }
-        }
+    const chars = map.get(number);
+    if (index >= digitArr.length) {
+      result.push(letter);
+    } else {
+      for (const c of chars) {
+        bt(index + 1, letter + c);
+      }
     }
+  }
 
-    bt();
+  bt();
 
-    return result;
+  return result;
 };
 
-console.log(letterCombinations('23'))
+console.log(letterCombinations("235"));
